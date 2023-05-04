8c6c602466d3126a11acdb067180e5e5fe840b42
schen818 <103452317+schen818@users.noreply.github.com>
2023-05-03 23:20:46 -0400

    Add files via upload

---
+++ teensyCode/defaultOperation/README.txt
@@ -0,0 +1,1 @@
+Default operation simply moves the robot in response to JSON message commands. It also returns battery status information when requested from a JSON message command.
---
+++ teensyCode/defaultOperationUART/defaultOperationUART.ino
@@ -0,0 +1,28 @@
+#include <GritsbotXUART.h>
+
+GritsbotXUART myRobot;
+int garbage[2];
+
+void setup() {
+  Serial1.begin(500000);
+  myRobot.SETUP();
+  myRobot.rainbow(10);
+  myRobot.turnOffLED();
+  myRobot.disableIR();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  myRobot.getEncoderCounts(garbage);
+  myRobot.checkCharging();
+  if (myRobot.checkCharging()){ //If we're charging disable unnecessary current draws.
+    myRobot.disableIR();
+    //myRobot.turnOffLED();
+  }
+  myRobot.checkBattVoltage();
+
+  myRobot.jsonSerialRead();
+  myRobot.communicationCheck(500);
+  myRobot.followCommands();  
+}
